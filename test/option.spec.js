e73e80f01a0f47bbb73c58647ab99345041da297
Fangli Xie <xfloops@gmail.com>
2018-12-27 19:33:05 +0800

    fix: resolve nested template parse

--- test/option.spec.js
+++ test/option.spec.js
@@ -19,6 +19,24 @@ describe('parseOption', function () {
 
     expect(parseOption(tmpl)).to.deep.equal(result)
   })
+
+  it('should parse nested <template> block', function () {
+    let tmpl = `
+      <template><div>hello</div><template>2222222</template></template>
+      <script>
+        c = {
+          data: 123
+        }
+      </script>
+    `
+    let result = {
+      template: `<div>hello</div><template>2222222</template>`,
+      data: 123
+    }
+
+    expect(parseOption(tmpl)).to.deep.equal(result)
+  })
+
 })
 
 
